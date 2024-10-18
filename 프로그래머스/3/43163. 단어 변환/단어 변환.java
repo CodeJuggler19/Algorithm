2297c7f55766960f1e9648c76ce3eddbc88faea7
Jinyongmin <jym7546@gmail.com>
2024-10-18 17:46:14 +0900

    [level 3] Title: 단어 변환, Time: 1.99 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,64 +1,55 @@
 import java.util.*;
 
-class Solution {
-    class Node{
-        String word;
-        int len;
-        
-        public Node(String word, int len){
-            this.word = word;
-            this.len = len;
-        }
-        String getWord(){
-            return this.word;
-        }
-        int getLen(){
-            return this.len;
-        }
-    }
-    int answer;
+public class Solution {
     public int solution(String begin, String target, String[] words) {
-        answer = 0;
-        
-        Queue<Node> q = new LinkedList<>();
-        
-        q.add(new Node(begin, 0));
-        
-        while(!q.isEmpty()){
-            Node current = q.poll();
-			// 추가한 부분
-		    if(current.getLen() > words.length){
-			    break;
-		    }
-            if(current.getWord().equals(target)){
-                answer = current.getLen();
-                break;
+        Set<String> wordSet = new HashSet<>(Arrays.asList(words));
+        Set<String> visited = new HashSet<>();
+        int answer = 0;
+
+        int wordSize = words[0].length();
+
+        // 큐에 시작 단어와 변환 횟수를 담음
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(begin, 0));
+        visited.add(begin);
+
+        while (!queue.isEmpty()) {
+            Pair current = queue.poll();
+            String word = current.word;
+            int count = current.count;
+
+            // 목표 단어에 도달한 경우
+            if (word.equals(target)) {
+                return count;
             }
-            for(int i=0; i<words.length; i++){
-                boolean check = checkWord(current.getWord(), words[i]);
-                if(check){
-                    q.add(new Node(words[i], current.getLen()+1));
+
+            // 단어의 각 문자를 하나씩 바꿔가며 탐색
+            for (int i = 0; i < wordSize; i++) {
+                for (char j = 'a'; j <= 'z'; j++) {
+                    char[] tempArr = word.toCharArray();
+                    tempArr[i] = j;
+                    String temp = new String(tempArr);
+
+                    // 바꾼 단어가 words 목록에 있고, 아직 방문하지 않은 경우
+                    if (wordSet.contains(temp) && !visited.contains(temp)) {
+                        queue.offer(new Pair(temp, count + 1));
+                        visited.add(temp);
+                    }
                 }
             }
         }
-        
-        return answer;
+
+        return answer; // target에 도달하지 못한 경우
     }
-    boolean checkWord(String a, String b){
-        int cnt = 0;
-        for(int i=0; i<a.length(); i++){
-            if(a.charAt(i) != b.charAt(i)){
-                cnt++;
-            }
-            if(cnt>1){
-                break;
-            }
-        }
-        if(cnt < 2){
-            return true;
-        }else{
-            return false;
+
+    // 단어와 변환 횟수를 저장할 클래스
+    class Pair {
+        String word;
+        int count;
+
+        Pair(String word, int count) {
+            this.word = word;
+            this.count = count;
         }
     }
-    
 }
