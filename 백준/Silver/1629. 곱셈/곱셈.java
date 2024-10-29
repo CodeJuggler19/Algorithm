d96d749884e1999a47d85a89904ea64c87009ac4
Jinyongmin <jym7546@gmail.com>
2024-10-29 15:30:02 +0900

    [Silver I] Title: 곱셈, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,39 +1,38 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int[] result;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
 
-        long result = modularExponentiation(A, B, C);  // 모듈러 지수법 함수 호출
-        bw.write(String.valueOf(result));
 
+        bw.write(String.valueOf(modulation(A, C, B)));
         bw.flush();
         bw.close();
         br.close();
     }
 
-    // 모듈러 지수법 (Modular Exponentiation)
-    public static long modularExponentiation(long base, long exp, long mod) {
-        long result = 1;
-        base = base % mod;  // A % C를 먼저 계산
-
-        while (exp > 0) {
-            // 지수(exp)가 홀수일 때는 결과에 base를 곱해줌
-            if (exp % 2 == 1) {
-                result = (result * base) % mod;
-            }
-            // 지수를 반으로 나누고 base는 제곱
-            exp = exp >> 1;
-            base = (base * base) % mod;
+    static long modulation(long A, long C, long pow) {
+        if (pow == 1) {
+            return A % C;
         }
-        return result;
+
+        long temp = modulation(A, C, pow / 2);
+
+        if (pow % 2 == 1) {
+            return (((temp * temp) % C) * A) % C;
+        }
+
+        return (temp * temp) % C;
     }
+
 }
