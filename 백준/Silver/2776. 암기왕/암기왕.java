ac2f8e049d6cedd6c411672fad5f4788542911c0
Jinyongmin <jym7546@gmail.com>
2025-01-16 21:56:10 +0900

    [Silver IV] Title: 암기왕, Time: 1312 ms, Memory: 333980 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -4,31 +4,48 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int T = Integer.parseInt(br.readLine());
 
-        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         while (T-- > 0) {
-            Set<Integer> list = new HashSet<>();
             int N = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) {
-                list.add(Integer.parseInt(st.nextToken()));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int[] note = new int[N];
+            for(int i = 0; i < N; i++){
+                note[i] = Integer.parseInt(st.nextToken());
             }
+            Arrays.sort(note);
+
             int M = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < M; i++) {
-                int cur = Integer.parseInt(st.nextToken());
-                if (list.contains(cur)) {
-                    bw.write(1 + "\n");
-                } else {
-                    bw.write(0 + "\n");
+
+
+            for(int i = 0; i < M; i++){
+                int num = Integer.parseInt(st.nextToken());
+                int low = 0;
+                int high = N - 1;
+                boolean find = false;
+                while(low <= high){
+                    int mid = (low + high) / 2;
+                    if(note[mid] == num){
+                        find = true;
+                        break;
+                    }
+                    if(note[mid] > num){
+                        high = mid - 1;
+                    }else{
+                        low = mid + 1;
+                    }
+                }
+                if(find){
+                    sb.append(1).append("\n");
+                }else{
+                    sb.append(0).append("\n");
                 }
             }
         }
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
         br.close();
     }
 }
