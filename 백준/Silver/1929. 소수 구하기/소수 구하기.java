a17f98f5ab27e9047f32e598128c2bf830c0e5c8
Jinyongmin <jym7546@gmail.com>
2025-02-07 22:37:54 +0900

    [Silver III] Title: 소수 구하기, Time: 176 ms, Memory: 25644 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -2,34 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int s = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
 
         boolean[] prime = new boolean[1000001];
 
         prime[0] = prime[1] = true;
 
-        for (int i = 2; i <= (int)Math.sqrt(1000000); i++) {
-            if (prime[i]) {
+        for(int i = 2; i <= Math.sqrt(1000001); i++){
+            if(prime[i]){
                 continue;
             }
-            for (int j = i * i; j < prime.length; j = j + i){
+            for(int j = i * i; j < prime.length; j = j + i){
                 prime[j] = true;
             }
         }
-        for(int i = s; i <= e; i++){
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = M; i<=N; i++){
             if(!prime[i]){
-                bw.write(i + "\n");
+                sb.append(i + "\n");
             }
         }
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
         br.close();
     }
 }
