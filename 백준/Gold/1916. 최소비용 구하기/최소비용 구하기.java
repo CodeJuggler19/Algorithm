44ef8ae5d401c5a77a850bd575a1d4b6450a2aff
Jinyongmin <jym7546@gmail.com>
2025-02-25 21:47:52 +0900

    [Gold V] Title: 최소비용 구하기, Time: 428 ms, Memory: 52228 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -3,77 +3,73 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static class Node implements Comparable<Node> {
+        int end, cost;
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-
-        List<List<Node>> graph = new ArrayList<>();
-
-        StringTokenizer st;
-
-        boolean[] visited = new boolean[n + 1];
-        int[] costs = new int[n + 1];
-
-        Arrays.fill(costs, Integer.MAX_VALUE);
-
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            graph.get(start).add(new Node(end, cost));
-        }
-
-        PriorityQueue<Node> q = new PriorityQueue<>();
-        st = new StringTokenizer(br.readLine());
-        int start = Integer.parseInt(st.nextToken());
-        int end = Integer.parseInt(st.nextToken());
-        costs[start] = 0;
-
-        q.add(new Node(start, 0));
-        while (!q.isEmpty()) {
-            Node curNode = q.poll();
-            int current = curNode.getEnd();
-            if (!visited[current]) {
-                visited[current] = true;
-                for (Node node : graph.get(current)) {
-                    if (!visited[node.getEnd()] && costs[node.getEnd()] > costs[current] + node.getCost()) {
-                        costs[node.getEnd()] = costs[current] + node.getCost();
-                        q.add(new Node(node.getEnd(), costs[node.getEnd()]));
-                    }
-                }
-
-            }
-        }
-        System.out.println(costs[end]);
-    }
-
-    public static class Node implements Comparable<Node> {
-        int end;
-        int cost;
-
-        public Node(int end, int cost) {
+        public Node( int end, int cost) {
             this.end = end;
             this.cost = cost;
         }
 
-        public int getEnd() {
-            return end;
-        }
-
-        public int getCost() {
-            return cost;
-        }
-
         @Override
         public int compareTo(Node node) {
-            return cost - node.cost;
+            return this.cost - node.cost;
         }
     }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        List<List<Node>> list = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            list.get(s).add(new Node(e, c));
+        }
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        boolean[] vis = new boolean[N + 1];
+        int[] cost = new int[N + 1];
+        Arrays.fill(cost, Integer.MAX_VALUE);
+
+
+        cost[start] = 0;
+        Queue<Node> q = new PriorityQueue<>();
+        q.add(new Node(start, 0));
+
+        while(!q.isEmpty()){
+            Node cur = q.poll();
+            if(!vis[cur.end]){
+                vis[cur.end] = true;
+                for(Node c : list.get(cur.end)){
+                    if(!vis[c.end]){
+                        cost[c.end] = Math.min(cost[c.end] , cost[cur.end] + c.cost);
+                        q.add(new Node(c.end, cost[c.end]));
+                    }
+                }
+            }
+
+
+        }
+
+
+        System.out.println(cost[end]);
+        br.close();
+    }
+
+
 }
