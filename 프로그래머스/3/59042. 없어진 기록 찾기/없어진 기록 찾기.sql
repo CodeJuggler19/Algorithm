900c65952457006fc483e1e97201603664babde7
Jinyongmin <jym7546@gmail.com>
2025-02-26 23:44:17 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,5 +1,36 @@
--- 코드를 입력하세요
-SELECT o.animal_id, o.name
-from animal_outs o
-left join animal_ins i on o.animal_id = i.animal_id
-where i.animal_id is NULL;
+# select outs.animal_id, outs.name
+# from animal_outs as outs
+# left join animal_ins as ins on ins.animal_id = outs.animal_id
+# where ins.animal_id is null
+
+
+
+SELECT OUTS.ANIMAL_ID, OUTS.NAME
+FROM ANIMAL_OUTS AS OUTS
+LEFT JOIN ANIMAL_INS AS INS ON OUTS.ANIMAL_ID = INS.ANIMAL_ID
+WHERE INS.ANIMAL_ID IS NULL;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
