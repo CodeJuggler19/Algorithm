6ad9634e558080be33d463e4ea2c23dfe406e561
Jinyongmin <jym7546@gmail.com>
2025-03-07 12:42:09 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,36 +1,4 @@
-# select outs.animal_id, outs.name
-# from animal_outs as outs
-# left join animal_ins as ins on ins.animal_id = outs.animal_id
-# where ins.animal_id is null
-
-
-
 SELECT OUTS.ANIMAL_ID, OUTS.NAME
 FROM ANIMAL_OUTS AS OUTS
 LEFT JOIN ANIMAL_INS AS INS ON OUTS.ANIMAL_ID = INS.ANIMAL_ID
 WHERE INS.ANIMAL_ID IS NULL;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
