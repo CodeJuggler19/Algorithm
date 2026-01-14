982e368443f4fe35c51a660e1ce26dadb7945650
Jinyongmin <jym7546@gmail.com>
2026-01-14 22:58:46 +0900

    [Silver V] Title: CD, Time: 888 ms, Memory: 298036 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.java
+++ 백준/Silver/4158. CD/CD.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,22 +11,31 @@ public class Main {
             int M = Integer.parseInt(st.nextToken());
 
             if(N == 0 & M == 0) break;
-            Set<Integer> set = new HashSet<>();
-            for(int i = 0; i < N; i++){
-                int num = Integer.parseInt(br.readLine());
-                set.add(num);
-            }
 
             int result = 0;
+            int[] list = new int[N];
+            for(int i = 0; i < N; i++){
+                list[i] = Integer.parseInt(br.readLine());
+            }
             for(int i = 0; i < M; i++){
                 int num = Integer.parseInt(br.readLine());
-                if(set.contains(num)){
-                    result ++;
+                int left = 0;
+                int right = N - 1;
+                while(left <= right){
+                    int mid = (left + right) / 2;
+
+                    if(list[mid] > num){
+                        right = mid - 1;
+                    }else if(list[mid] < num){
+                        left = mid + 1;
+                    }else{ // list[mid] == num
+                        result ++;
+                        break;
+                    }
                 }
             }
             System.out.println(result);
         }
-
         br.close();
     }
 }
