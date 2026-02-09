114dd807b698123b17c8ccda25a1fc05957598dd
Jinyongmin <jym7546@gmail.com>
2026-02-09 10:18:48 +0900

    [Gold IV] Title: 리모컨, Time: 404 ms, Memory: 265100 KB -BaekjoonHub

---
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int curCh = 100;
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		boolean[] check = new boolean[10];
+		if(M != 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			while(st.hasMoreTokens()) {
+				check[Integer.parseInt(st.nextToken())] = true;
+			}
+		}
+		
+		int min = Integer.MAX_VALUE;
+		
+		min = Math.abs(curCh - N); 
+		
+		for(int i = 0; i <= 1000000; i++) {
+			String numStr = String.valueOf(i);
+			boolean yn = true;
+			for(char num : numStr.toCharArray()) {
+				if(check[Integer.parseInt(String.valueOf(num))]) {
+					yn = false;
+					break;
+				}
+			}
+			
+			if(yn) {
+				int pow = numStr.length();
+				int diff = Math.abs(N - Integer.parseInt(numStr));
+				min = Math.min(min, pow + diff);
+			}
+		}
+		
+		System.out.println(min);
+			
+		br.close();
+	}
+}
