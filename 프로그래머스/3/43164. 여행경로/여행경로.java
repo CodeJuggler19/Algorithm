084a4cd8a910526e7ba5abe7db422a3b0be524fd
Jinyongmin <jym7546@gmail.com>
2026-07-22 21:54:27 +0900

    [level 3] Title: 여행경로, Time: 43.77 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    static List<String> list = new ArrayList<>();
+    static boolean[] useTicket;
+    public String[] solution(String[][] tickets) {
+        String[] answer = {};
+        
+        useTicket = new boolean[tickets.length];
+        
+        dfs(0, "ICN", "ICN", tickets);
+        
+        Collections.sort(list);
+        return list.get(0).split(" ");
+    }
+    
+    public void dfs(int depth, String now, String path, String[][] tickets){
+        if(depth == tickets.length){
+            list.add(path);
+            return;
+        }
+        
+        for(int i = 0; i < tickets.length; i++){
+            if(!useTicket[i] && now.equals(tickets[i][0])){
+                useTicket[i] = true;
+                dfs(depth + 1, tickets[i][1], path + " " + tickets[i][1], tickets);
+                useTicket[i] = false;
+            }
+        }
+    }
+}
